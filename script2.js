97e6abb28b84c82f25cfe2ef2de465960329705a
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-03-03 17:14:54 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,26 @@
+const planeta = document.getElementById("planeta");
+const raioElement = document.getElementById("raio");
+const massaElement = document.getElementById("massa");
+let velocidade = 0;
+let massa = massaElement.value.replace(",", ".");
+let raio = raioElement.value;
+let radiano = 0;
+const orbita = document.getElementById("orbita");
+
+orbita.style.height = `${raio}vmin`;
+orbita.style.width = `${raio}vmin`;
+
+setInterval(() => {
+  planeta.style.left = `${Math.cos(radiano) * raio}vmin`;
+  planeta.style.top = `${Math.sin(radiano) * raio}vmin`;
+  velocidade = massa/raio;
+  radiano += velocidade * massa;
+}, 1000 / 60);
+
+function mudarValores() {
+  raio = raioElement.value;
+  massa = massaElement.value.replace(",", ".");
+  orbita.style.height = `${raio}vmin`;
+  orbita.style.width = `${raio}vmin`;
+  console.log("valores", { raio, massa });
+}
