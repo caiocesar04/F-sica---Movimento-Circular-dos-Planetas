9c46bca79f67cb49909f454e40f20e1f5a6e2dbb
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-03-17 16:52:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const planeta = document.getElementById("planeta");
-const velocidadeElement = document.getElementById("velocidade");
 const raioElement = document.getElementById("raio");
 const massaElement = document.getElementById("massa");
-let velocidade = velocidadeElement.value.replace(",", ".");
+const tempoElement = document.getElementById("tempo");
+const gravitacional = 5;
+let velocidade = 0;
 let massa = massaElement.value.replace(",", ".");
 let raio = raioElement.value;
 let radiano = 0;
+let tempo = 0;
+let espacoPercorrido = 0;
 const orbita = document.getElementById("orbita");
 
 orbita.style.height = `${raio}vmin`;
@@ -14,14 +17,26 @@ orbita.style.width = `${raio}vmin`;
 setInterval(() => {
   planeta.style.left = `${Math.cos(radiano) * raio}vmin`;
   planeta.style.top = `${Math.sin(radiano) * raio}vmin`;
-  radiano += velocidade * massa;
-}, 1000 / 60);
+  velocidade =  Math.sqrt ( gravitacional * massa/raio);
+  document.getElementById("velocidade").innerText=velocidade.toFixed(2);
+  radiano += velocidade * ( [gravitacional * massa/raio] / 100000);
+  
+},1000 / 60); 
+setInterval(() => {
+ tempo++;
+ tempoElement.innerText=tempo;
+ espacoPercorrido = (2 * 3.14) * raio;
+ document.getElementById("espacoPercorrido").innerText=espacoPercorrido.toFixed(2);
+},999 ) 
+
 
 function mudarValores() {
+  if(tempo != null){
+    tempo = null;
+    }
   raio = raioElement.value;
-  velocidade = velocidadeElement.value.replace(",", ".");
   massa = massaElement.value.replace(",", ".");
   orbita.style.height = `${raio}vmin`;
   orbita.style.width = `${raio}vmin`;
-  console.log("valores", { raio, velocidade, massa });
+  console.log("valores", { raio, massa });
 }
